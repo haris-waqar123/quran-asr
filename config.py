cb358528ae31ccde774ded57100daae32ce0b7b0
Muhammad Haris Waqar <haris.waqar@9dtechnologies.net>
2024-11-11 12:59:14 +0500

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-
-HUGGINGFACE_TOKEN = os.getenv("HUGGINGFACE_HUB_TOKEN", "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")
-AUDIO_FILES_DIRECTORY = "audio_files/"
-
-API_KEY = "s2yHZSwIfhm1jUo01We00c9APLAndXgX"
